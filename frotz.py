bfa2b610451f09713aa441a4b45d032d724882dc
Aaron Chantrill <aaron.chantrill@dottywood.org>
2022-11-09 08:44:49 -0500

    Restore support for old mic.active_listen() method

--- frotz.py
+++ frotz.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
-import os.path
+import os
 from . import textPlayer as tp
 from naomi import plugin
 
@@ -41,6 +41,7 @@ class FrotzPlugin(plugin.SpeechHandlerPlugin):
     def handle(self, intent, mic, *args):
         _ = self.gettext
         text = intent['input']
+        player = intent.get('user', '')
         self._mic = mic
         self._logger = logging.getLogger(__name__)
         # pdb.set_trace()
@@ -58,7 +59,7 @@ class FrotzPlugin(plugin.SpeechHandlerPlugin):
             "games",
             self.game+".sav"
         )
-        self._mic.say(self.gettext("beginning "+self.game_name))
+        self._mic.say_sync(self.gettext("beginning "+self.game_name))
         # open zork1.corpus in the current directory and create
         # a phrase list from it
         corpus = os.path.join(
@@ -83,54 +84,69 @@ class FrotzPlugin(plugin.SpeechHandlerPlugin):
                 t.restore(self.savefile)
                 response = t.execute_command('l')
             current_location = response.location
-            self._mic.say(response.location)
+            self._mic.say_sync(response.location)
             if(response.description):
-                self._mic.say(response.description)
+                self._mic.say_sync(response.description)
 
             mode_not_stopped = True
             while mode_not_stopped:
-                texts = self._mic.active_listen()
-
+                sr_response = self._mic.active_listen(play_prompts=False)
+                if isinstance(sr_response, dict):
+                    texts = sr_response.get('utterance', [])
+                    user = sr_response.get('speaker', '')
+                else:
+                    texts = sr_response
+                    user = ''
                 text = ''
                 if texts:
                     text = ', '.join(texts).upper()
+                # Let anyone quit the game in case it gets left on.
+                if((user == player) or ("QUIT" in text)):
+                    if not text:
+                        # mic.say(_('Pardon?'))
+                        continue
 
-                if not text:
-                    # mic.say(_('Pardon?'))
-                    continue
+                    # Here I have the chance to catch
+                    # some commonly misheard phrases
+                    if("GO SELF" in text):
+                        self._logger.debug('GO SELF corrected to GO SOUTH')
+                        text = "GO SOUTH"
+                    if("WHERE ROBE" in text):
+                        self._logger.debug('WHERE ROBE corrected to WEAR ROBE')
+                        text = "WEAR ROBE"
 
-                # Here I have the chance to catch
-                # some commonly misheard phrases
-                if("GO SELF" in text):
-                    self._logger.debug('GO SELF corrected to GO SOUTH')
-                    text = "GO SOUTH"
-                if("WHERE ROBE" in text):
-                    self._logger.debug('WHERE ROBE corrected to WEAR ROBE')
-                    text = "WEAR ROBE"
-
-                if("QUIT" in text):
-                    mode_not_stopped = False
-                else:
-                    response = t.execute_command(text)
-                    say_location = False
-                    # Don't state the location at the beginning of each
-                    # response
-                    # For the most part, only state the location when
-                    # the location changes or if I ask for "look" or
-                    # "where am i"
-                    if(current_location != response.location):
-                        current_location = response.location
-                        say_location = True
-                    if(text == ['LOOK'] or text == ['WHERE AM I']):
-                        say_location = True
-                    if(not response.description):
-                        say_location = True
-                    if(say_location):
-                        self._mic.say(response.location)
-                    if(response.description):
-                        self._mic.say(response.description)
+                    if("QUIT" in text):
+                        mode_not_stopped = False
+                    else:
+                        response = t.execute_command(text)
+                        say_location = False
+                        # For some reason, every error condition reports the
+                        # location as "West of House". Luckily, error messages
+                        # also always seem to begin with a space.
+                        if (response.description[0] != " "):
+                            # Don't state the location at the beginning of each
+                            # response
+                            # For the most part, only state the location when
+                            # the location changes or if I ask for "look" or
+                            # "where am i"
+                            if(current_location != response.location):
+                                current_location = response.location
+                                say_location = True
+                            if(text == ['LOOK'] or text == ['WHERE AM I']):
+                                say_location = True
+                            if(not response.description):
+                                say_location = True
+                        if(say_location):
+                            self._mic.say_sync(response.location)
+                        if(response.description):
+                            self._mic.say_sync(response.description)
 
         self._mic.say(self.gettext('Saving game'))
+        # for some reason, the t.save method silently refuses to overwrite a
+        # save game, so we have to remove the old game file first.
+        # Ideally we would rotate the save files so the user has a history.
+        if os.path.isfile(self.savefile):
+            os.remove(self.savefile)
         t.save(self.savefile)
         t.quit()
         self._mic.say(self.gettext('Ending game'))
